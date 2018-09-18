56063f511c1eb22dfd77ca1208bf2daa4ae96f7d
Eduardo Graziano <eduardo@Eduardos-MBP.gsvl.fl.wtsky.net>
2018-09-18 16:07:26 -0400

    uploading Album App

---
+++ albums/src/components/header.js
@@ -0,0 +1,37 @@
+//Import Libraries for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+//Make a component
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  }
+};
+
+//Making a component available to other parts of the app by exporting it
+//since it is rendered in the root Component
+export default Header;
